8716773bb1c45fd01f44e4c670a11be1b0298043
Mohammad Bahaa <MohammadBahaa_2001mb@outlook.com>
2020-02-17 03:16:33 +0200

    Updates

---
+++ precache-manifest.fa087a3b23f2d7079aa7e3510575902f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e4229d23ecbc8e709828850e4d9dac",
+    "url": "/crwn-ecommerce/index.html"
+  },
+  {
+    "revision": "6d2b7b567bf4dc6c4ca5",
+    "url": "/crwn-ecommerce/static/css/main.545a5e28.chunk.css"
+  },
+  {
+    "revision": "5bbc561f1415cc15ef3c",
+    "url": "/crwn-ecommerce/static/js/2.c2b5a27f.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/crwn-ecommerce/static/js/2.c2b5a27f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d2b7b567bf4dc6c4ca5",
+    "url": "/crwn-ecommerce/static/js/main.bd88c30b.chunk.js"
+  },
+  {
+    "revision": "cdd10374990bede487f3",
+    "url": "/crwn-ecommerce/static/js/runtime-main.1facb0f7.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
